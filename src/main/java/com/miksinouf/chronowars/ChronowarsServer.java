4e892ffda97d53a89f80c8b7e802e047b8820efa
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-06-08 15:11:05 +0200

    access static file

--- src/main/java/com/miksinouf/chronowars/ChronowarsServer.java
+++ src/main/java/com/miksinouf/chronowars/ChronowarsServer.java
@@ -1,9 +1,11 @@
 package com.miksinouf.chronowars;
 
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 public class ChronowarsServer {
   public static void main(String[] args) {
+      staticFileLocation("/web-resources");
       get("/hello/:name", (request, response) -> "hello " + request.params("name"));
   }
 }
