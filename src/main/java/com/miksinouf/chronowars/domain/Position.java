8025fb9dcb4d52f7012bebafe97e78316e659347
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-06-08 16:37:52 +0200

    create position class

---
+++ src/main/java/com/miksinouf/chronowars/domain/Position.java
@@ -0,0 +1,18 @@
+package com.miksinouf.chronowars.domain;
+
+public class Position {
+    private Integer x, y;
+
+    public Position(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+}
