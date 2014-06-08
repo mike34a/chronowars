e474614de77ee88ab6e0546fc042fad5e87e5247
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-06-09 00:30:42 +0200

    let access web client to new game subscription and start

--- src/main/java/com/miksinouf/chronowars/ChronowarsServer.java
+++ src/main/java/com/miksinouf/chronowars/ChronowarsServer.java
@@ -3,9 +3,13 @@ package com.miksinouf.chronowars;
 import static spark.Spark.get;
 import static spark.SparkBase.staticFileLocation;
 
+import com.miksinouf.chronowars.domain.games.GameQueueSingleton;
+
 public class ChronowarsServer {
   public static void main(String[] args) {
       staticFileLocation("/web-resources");
       get("/hello/:name", (request, response) -> "hello " + request.params("name"));
+      get("/gametoken/:nickname", (request, response) -> GameQueueSingleton.INSTANCE.addNewPlayer(request.params("nickname")));
+      get("/startnewgame", (request, response) -> GameQueueSingleton.INSTANCE.runNextGame());
   }
 }
