c185c3969095b6f3bfc54776ca6ef9b0c25c66f7
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-06-30 01:06:36 +0200

    complete API

--- src/main/java/com/miksinouf/chronowars/ChronowarsServer.java
+++ src/main/java/com/miksinouf/chronowars/ChronowarsServer.java
@@ -10,20 +10,35 @@ public class ChronowarsServer {
 
   public static void main(String[] args) {
       staticFileLocation("/web-resources");
-      get("/hello/:name", (request, response) -> "hello " + request.params("name"));
+
+      //permet d'inscrire le joueur à la liste d'attente des parties et renvoie l'identifiant unique
       get("/register/:nickname", (request, response) -> GameQueueSingleton.INSTANCE.register(request.params("nickname")));
+
+      //identifiant de la partie ou message qui indique qu'aucune partie n'a démarré
       get("/have_i_running_game/:identifier", (request, response) -> GameQueueSingleton.INSTANCE
               .hasPlayerAGame(request.params("identifier"))
               .map(identity())
               .orElse("No game started yet."));
-      
+
+      /*renvoie une réponse json de la forme
+                 {"currentColorToPlay": "BLACK",
+                  "status": "running",
+                  "blackTokens": "(2,2),(1,3)",
+                  "whiteTokens": "(1,2),(2,5),(3,2)"
+                  }
+       */
+      get("/get_game/:gameIdentifier", (request, response) -> GameQueueSingleton.INSTANCE
+              .hasPlayerAGame(request.params("gameIdentifier")));
+
+      //permet de placer un pion
       put("/set_token/:gameIdentifier/:playerIdentifier/:x/:y", (request, response) -> GameQueueSingleton.INSTANCE.setToken(
               request.params("gameIdentifier"),
               request.params("playerIdentifier"),
               request.params("x"),
               request.params("y")
       ));
-      
+
+      //permet de déplacer un pion
       patch("/move_token/:gameIdentifier/:playerIdentifier/:x/:y/:moveDirection", (request, response) -> GameQueueSingleton.INSTANCE.moveToken(
               request.params("gameIdentifier"),
               request.params("playerIdentifier"),
