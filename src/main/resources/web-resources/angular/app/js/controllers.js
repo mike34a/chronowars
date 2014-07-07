5f835a4a1b83f2afb00bff03ed813559eb069e24
logar <simon.boeuf@gmail.com>
2014-07-07 08:26:42 +0200

    Full api ready to use in angular

--- src/main/resources/web-resources/angular/app/js/controllers.js
+++ src/main/resources/web-resources/angular/app/js/controllers.js
@@ -8,17 +8,47 @@ chronoWarsControllers.controller(
 		'PhoneListCtrl', 
 		['$scope', 'launchGame','$http',
 		function($scope, launchGame, $http) {
+			
+			/*API*/
 			// $scope.getPlayerId = launchGame.getPlayerId; TODO : We should do this but promises won't let us
-			$scope.getPlayerId = function(name) {
-				return $http.get('/register/' + name).then(function(result) {
-					$scope.playerId = result.data;
+			$scope.registerPlayer = function(name) {
+				$http.get('/register/' + name).then(function(response) {
+					$scope.playerId = response.data;
 				});
-			}
+			};
+			
+			$scope.getGameId = function(playerId) {
+				$http.get('/have_i_running_game/' + playerId).then(function(response) {
+					$scope.gameId = response.data;
+				})
+			};
+			
+			$scope.getBoard = function(gameId) {
+				$http.get('/get_game/' + gameId).then(function(response) {
+					$scope.board = response.data;
+				})
+			};
+			
+			$scope.setToken = function(playerId, gameId, row, col) {
+				$http.put('/set_token/' + gameId + '/' + row + '/' + '/' + col).then(function(response) {
+					$scope.board = response.data;
+				})
+			};
+			
+			$scope.moveToken = function(playerId, gameId, row, col, direction) {
+				$http.patch('/set_token/' + gameId + '/' + row + '/' + '/' + col + '/' + direction).then(function(response) {
+					$scope.board = response.data;
+				})
+			};
+			
+			/*Game*/
+			
+			/*
 			$("#tatami").draggable({
 				revert : "invalid"
 			});
 			$("#a2").droppable();
 			$("#a1").droppable();
-		}
-		]
+			*/
+		}]
 );
