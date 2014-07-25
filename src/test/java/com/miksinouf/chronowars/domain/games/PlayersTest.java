cb20048093e0bbc127a91eaf60f1b0424fa57dfe
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-07-25 08:12:50 +0200

    proper exception testing with junit rules

--- src/test/java/com/miksinouf/chronowars/domain/games/PlayersTest.java
+++ src/test/java/com/miksinouf/chronowars/domain/games/PlayersTest.java
@@ -1,13 +1,14 @@
 package com.miksinouf.chronowars.domain.games;
 
 import static com.miksinouf.chronowars.domain.board.MoveResultType.SUCCESS;
-import static com.miksinouf.chronowars.domain.board.MoveResultType.TOKEN_OUT_OF_BOUNDS;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.miksinouf.chronowars.domain.board.Board;
 import com.miksinouf.chronowars.domain.board.IllegalMoveException;
@@ -17,18 +18,23 @@ import com.miksinouf.chronowars.domain.player.Player;
 import com.miksinouf.chronowars.domain.player.UnknownPlayerException;
 
 public class PlayersTest {
-    
+
     private Players players;
     private Player whitePlayer;
     private Player blackPlayer;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
         players = new Players();
         final Board board = new Board();
 
-        whitePlayer = new Player("whitePlayerNick", Color.WHITE, 2, "whitePlayerIdentifier", board);
-        blackPlayer = new Player("blackPlayerNick", Color.WHITE, 2, "blackPlayerIdentifier", board);
+        whitePlayer = new Player("whitePlayerNick", Color.WHITE, 2,
+                "whitePlayerIdentifier", board);
+        blackPlayer = new Player("blackPlayerNick", Color.WHITE, 2,
+                "blackPlayerIdentifier", board);
 
         players.addPlayers(whitePlayer, blackPlayer);
     }
@@ -39,35 +45,32 @@ public class PlayersTest {
         whitePlayer = mock(Player.class);
         blackPlayer = mock(Player.class);
 
-        //WHEN
+        // WHEN
         players.addPlayers(whitePlayer, blackPlayer);
 
-        //THEN
+        // THEN
         verify(whitePlayer).setOpponent(blackPlayer);
         verify(blackPlayer).setOpponent(whitePlayer);
     }
 
     @Test
     public void should_set_token_at_a_right_position() throws Exception {
-        assertThat(players.setToken("whitePlayerIdentifier", 1, 2)).isEqualTo(new MoveResult(SUCCESS, 1, 2));
+        assertThat(players.setToken("whitePlayerIdentifier", 1, 2)).isEqualTo(
+                new MoveResult(SUCCESS, 1, 2));
     }
 
     @Test
-    public void should_throw_out_of_bounds_if_place_not_valid() throws Exception{
-    	Integer failed = 0;
-    	try {
-    		players.setToken("whitePlayerIdentifier", -1, 2);
-    	}
-    	catch (IllegalMoveException e){
-    		assertThat(e.invalidMoveResult).isEqualTo(new MoveResult(TOKEN_OUT_OF_BOUNDS, -1, 2));
-    		failed = 1;
-    	}
-    	assertThat(failed);
+    public void should_throw_out_of_bounds_if_place_not_valid() throws Exception {
+        expectedException.expect(IllegalMoveException.class);
+        expectedException.expectMessage("TOKEN_OUT_OF_BOUNDS at Position{x=-1, y=2}");
+
+        players.setToken("whitePlayerIdentifier", -1, 2);
     }
 
     @Test(expected = UnknownPlayerException.class)
     public void should_throw_error_if_player_does_not_exist() throws Exception {
-        
-        assertThat(players.setToken("invalidPlayerIdentifier", 1, 2)).isEqualTo(new MoveResult(SUCCESS, 1, 2));
+
+        assertThat(players.setToken("invalidPlayerIdentifier", 1, 2))
+                .isEqualTo(new MoveResult(SUCCESS, 1, 2));
     }
 }
