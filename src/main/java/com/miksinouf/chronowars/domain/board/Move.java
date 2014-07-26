499ab7898425310b7065efca550c578678fbdfc5
SimonBoeuf <simon.boeuf@gmail.com>
2014-07-27 00:18:58 +0200

    fixed a move bug

--- src/main/java/com/miksinouf/chronowars/domain/board/Move.java
+++ src/main/java/com/miksinouf/chronowars/domain/board/Move.java
@@ -1,13 +1,13 @@
 package com.miksinouf.chronowars.domain.board;
 
 public enum Move {
-    UP(0, -2),
-    UP_RIGHT(1, -1),
-    RIGHT(2, 0),
+    UP(-2, 0),
+    UP_RIGHT(-1, 1),
+    RIGHT(0, 2),
     DOWN_RIGHT(1, 1),
-    DOWN(0, 2),
-    DOWN_LEFT(-1, 1),
-    LEFT(-2, 0),
+    DOWN(2, 0),
+    DOWN_LEFT(1, -1),
+    LEFT(0, -2),
     UP_LEFT(-1, -1);
     
     public final int dx;
