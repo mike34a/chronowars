602f7e6a44b128733aa4679a3074fa663a292aa5
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-08-03 19:09:40 +0200

    remove unused code

--- src/main/java/com/miksinouf/chronowars/domain/games/GamesQueueSingleton.java
+++ src/main/java/com/miksinouf/chronowars/domain/games/GamesQueueSingleton.java
@@ -7,12 +7,10 @@ import java.util.Optional;
 import com.miksinouf.chronowars.domain.board.Board;
 import com.miksinouf.chronowars.domain.board.IllegalMoveException;
 import com.miksinouf.chronowars.domain.board.Move;
-import com.miksinouf.chronowars.domain.board.MoveResult;
 import com.miksinouf.chronowars.domain.player.*;
 
 public enum GamesQueueSingleton {
     INSTANCE;
-    public static final Integer MAX_NUMBER_OF_TOKENS = 5;
 
     private final Players players = new Players();
     private final SecureRandom secureRandom = new SecureRandom();
@@ -27,32 +25,39 @@ public enum GamesQueueSingleton {
         if (waitingPlayer.isPresent()) {
             Board board = new Board();
 
-            final Player whitePlayer = new Player(waitingPlayer.get(), board, Color.WHITE);
-            final Player blackPlayer = new Player(nickname, Color.BLACK, 8, playerIdentifier, board);
+            final Player whitePlayer = new Player(waitingPlayer.get(), board,
+                    Color.WHITE);
+            final Player blackPlayer = new Player(nickname, Color.BLACK, 8,
+                    playerIdentifier, board);
             players.addPlayers(whitePlayer, blackPlayer);
 
             this.waitingPlayer = Optional.empty();
         } else {
-            waitingPlayer = Optional.of(new WaitingPlayer(nickname, playerIdentifier));
+            waitingPlayer = Optional.of(new WaitingPlayer(nickname,
+                    playerIdentifier));
         }
-        
+
         return playerIdentifier;
     }
-    
+
     public synchronized String nextRandomIdentifier() {
         return new BigInteger(130, secureRandom).toString(32);
     }
-    
-    public synchronized String getGame(String playerIdentifier) throws UnknownPlayerException {
-    	return players.getGame(playerIdentifier);
+
+    public synchronized String getGame(String playerIdentifier)
+            throws UnknownPlayerException {
+        return players.getGame(playerIdentifier);
     }
 
-    public String setToken(String playerIdentifier, int x, int y) throws UnknownPlayerException, IllegalMoveException, TooManyTokensException {
+    public String setToken(String playerIdentifier, int x, int y)
+            throws UnknownPlayerException, IllegalMoveException,
+            TooManyTokensException {
         players.setToken(playerIdentifier, x, y);
         return "success";
     }
 
-    public String moveToken(String playerIdentifier, int x, int y, String move) throws UnknownPlayerException, IllegalMoveException {
+    public String moveToken(String playerIdentifier, int x, int y, String move)
+            throws UnknownPlayerException, IllegalMoveException {
         players.moveToken(playerIdentifier, x, y, Move.valueOf(move));
         return "success";
     }
