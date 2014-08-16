b4fb8df73aee81c4d4f1c2eedb0f4a4207786aed
SimonBoeuf <simon.boeuf@gmail.com>
2014-08-16 13:23:06 +0200

    interval set to 1 sec again

--- src/main/resources/web-resources/js/controllers.js
+++ src/main/resources/web-resources/js/controllers.js
@@ -18,7 +18,7 @@ chronoWarsControllers.controller('HomeCtrl', [
 							$location.path('/game/' + pidres);
 						}
 					});
-				}, 10);
+				}, 1000);
 			});
 		};
     } ]);
@@ -96,7 +96,7 @@ chronoWarsControllers.controller('GameCtrl', [
 				})
 				$scope.numberOfTokens = numberOfTokens;
 			});
-		}, 10);
+		}, 1000);
 
 		var placeToken = function(tileId) {
 			gameApi.setToken($routeParams.playerId,
